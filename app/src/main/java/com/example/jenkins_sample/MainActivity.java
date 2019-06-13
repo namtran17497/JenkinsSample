d0484d6417bc0344a9c158294d9feba676711ddc
namtran17497 <32796387+namtran17497@users.noreply.github.com>
2019-06-13 16:14:07 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/jenkins_sample/MainActivity.java
+++ app/src/main/java/com/example/jenkins_sample/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        superonCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
