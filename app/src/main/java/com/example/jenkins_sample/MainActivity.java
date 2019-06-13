0cf81eeb879ab009c67df86fd0bfd8625257592b
namtran17497 <32796387+namtran17497@users.noreply.github.com>
2019-06-13 16:29:14 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/jenkins_sample/MainActivity.java
+++ app/src/main/java/com/example/jenkins_sample/MainActivity.java
@@ -6,6 +6,7 @@ import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+    public String name = abc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
